8b8130a2bdfb2f24e7ef1cbb9815e844d6208f19
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-13 09:35:41 +0100

    Adding generation configs

---
+++ terrierutil/inference/generationconfig.py
@@ -0,0 +1,47 @@
+import json
+
+CONTRAST_CREATIVE = {
+    'top_k' : 10,
+    'do_sample' : True,
+    'penalty_alpha' : 0.5,
+    'temperature' : 2.0,
+    'top_p' : 0.75,
+}
+    
+BEAM_SEARCH = {
+    'num_beams' : 5,
+    'num_return_sequences' : 1,
+    'no_repeat_ngram_size' : 1,
+    'remove_invalid_values' : True,
+    'top_p' : 0.75,
+}
+    
+GREEDY_SMOOTH = {
+    'do_sample': False,
+    'temperature': 2.0,
+    'top_p' : 0.75,
+}
+
+CONTRAST_DETERMINISTIC = {
+    'top_k' : 10,
+    'do_sample' : False,
+    'penalty_alpha' : 0.5,
+    'temperature' : 0.1,
+    'top_p' : 0.75,
+}
+
+GREEDY_DETERMINISTIC = {
+    'do_sample': False,
+    'temperature': 0.1,
+    'top_p' : 0.75,
+}
+
+def load_generation_config(type, **kwargs):
+    types = {
+    "contrast_creative": CONTRAST_CREATIVE,
+    "beam_search":BEAM_SEARCH,
+    "greedy_smooth": GREEDY_SMOOTH,
+    "contrast_deterministic": CONTRAST_DETERMINISTIC,
+    "greedy_deterministic": GREEDY_DETERMINISTIC,
+    }
+    return json.dumps({**types[type], **kwargs})
